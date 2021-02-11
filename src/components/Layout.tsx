56652c8818376509e0842d2d19b6ef3be5285d5d
Nacho Iacovino <nacho.iacovino@gmail.com>
2021-02-11 15:05:52 +0100

    Add identity widget script

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,18 +1,20 @@
-import Head from "next/head";
-import Navigation from "./Navigation";
+import Head from 'next/head';
+
+import Navigation from './Navigation';
 
 type Props = {
   children: React.ReactNode;
 };
 export default function Layout({ children }: Props) {
   return (
-    <div className="root">
+    <div className='root'>
       <Head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="apple-touch-icon" href="/icon.png" />
-        <meta name="theme-color" content="#fff" />
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='manifest' href='/site.webmanifest' />
+        <link rel='apple-touch-icon' href='/icon.png' />
+        <meta name='theme-color' content='#fff' />
+        <script src='https://identity.netlify.com/v1/netlify-identity-widget.js'></script>
       </Head>
       <nav>
         <Navigation />
