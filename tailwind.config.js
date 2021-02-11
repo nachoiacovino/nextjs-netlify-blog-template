4c6db29ee9ab75d3f4eb17d79f2679224f99da42
Nacho Iacovino <nacho.iacovino@gmail.com>
2021-02-11 18:52:41 +0100

    Fix tailwind config to extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,22 +4,11 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: colors.black,
-      white: colors.white,
-      gray: colors.gray,
-      red: colors.red,
-      yellow: colors.yellow,
-      green: colors.green,
-      blue: colors.blue,
-      pink: colors.pink,
-      indigo: colors.indigo,
-      purple: colors.purple,
-      trueGray: colors.trueGray,
+    extend: {
+      colors: {
+        trueGray: colors.trueGray,
+      },
     },
-    extend: {},
   },
   variants: {
     extend: {},
