a2eae311a8e13a9a0752678c5d56d532485724d2
Nacho Iacovino <nacho.iacovino@gmail.com>
2021-02-11 15:56:00 +0100

    Import Hero component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 
-import Navigation from './Navigation';
+/* import Navigation from './Navigation'; */
 
 type Props = {
   children: React.ReactNode;
@@ -16,33 +16,10 @@ export default function Layout({ children }: Props) {
         <meta name='theme-color' content='#fff' />
         <script src='https://identity.netlify.com/v1/netlify-identity-widget.js'></script>
       </Head>
-      <nav>
+      {/*       <nav>
         <Navigation />
-      </nav>
+      </nav> */}
       <main>{children}</main>
-      <style jsx>
-        {`
-          .root {
-            display: block;
-            padding: 4rem 0;
-            box-sizing: border-box;
-            height: 100%;
-          }
-          main {
-            display: flex;
-            min-height: 100%;
-          }
-          @media (min-width: 769px) {
-            .root {
-              display: flex;
-              flex: 1 0 auto;
-            }
-            main {
-              flex: 1 0 auto;
-            }
-          }
-        `}
-      </style>
     </div>
   );
 }
